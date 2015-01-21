34fc3bfae900f7c50524c095593d95d19d7cc0ff
Alexei <alexeiatyahoodotcom@gmail.com>
2015-01-21 15:34:06 -0500

    Update WIP viewer app.

--- reviewer/app.py
+++ reviewer/app.py
@@ -9,16 +9,30 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 import dataset
 
 app = Flask(__name__)
 
 
-def get_crawl_breakdown(crawl_id):
-    statuses = []
+def get_problem_pages(crawl_ids):
+    if crawl_ids:
+        in_clause = "AND crawl_id IN (%s)" % ','.join(
+            [str(int(id)) for id in crawl_ids])
 
+    sql = """SELECT crawl_url, error, COUNT(*) count
+        FROM result
+        WHERE error IS NOT NULL %s
+        GROUP BY crawl_url, error
+        ORDER BY count DESC""" % (in_clause if crawl_ids else "")
+
+    with dataset.connect(app.config['DATABASE_URL']) as db:
+        result = db.query(sql)
+        return list(result)
+
+
+def get_crawl_breakdown(crawl_id):
     with dataset.connect(app.config['DATABASE_URL']) as db:
         result = db.query(
             """SELECT error, COUNT(DISTINCT crawl_url) AS num_urls
@@ -26,18 +40,10 @@ def get_crawl_breakdown(crawl_id):
             id=crawl_id
         ) if 'result' in db.tables else []
 
-        for row in result:
-            statuses.append({
-                'error': row['error'],
-                'num_urls': row['num_urls']
-            })
-
-    return statuses
+        return list(result)
 
 
 def get_crawls():
-    crawls = []
-
     with dataset.connect(app.config['DATABASE_URL']) as db:
         result = db.query(
             """SELECT
@@ -52,16 +58,7 @@ def get_crawls():
             ORDER BY crawl.id DESC"""
         ) if 'crawl' in db.tables else []
 
-        for row in result:
-            crawls.append({
-                'id': row['id'],
-                'num_urls': row['num_urls'],
-                'duration': row['duration'],
-                'start_time': datetime.utcfromtimestamp(
-                    int(row['start_time'])).strftime("%I:%M %p %b %d %Y"),
-            })
-
-    return crawls
+        return list(result)
 
 
 @app.route('/crawl/<int:crawl_id>')
@@ -75,7 +72,16 @@ def crawl(crawl_id):
 
 @app.route('/')
 def index():
+    crawls = get_crawls()
+
+    for crawl in crawls:
+        crawl['start_time'] = datetime.utcfromtimestamp(
+            int(crawl['start_time'])).strftime("%I:%M %p %b %d %Y")
+
     return render_template(
         'crawls.html',
-        crawls=get_crawls()
+        crawls=crawls,
+        problem_pages=get_problem_pages(
+            [int(i) for i in request.values.getlist('crawl')]
+        )
     )
---
+++ reviewer/static/index.js
@@ -0,0 +1,41 @@
+/*!
+ * chameleon-crawler
+ *
+ * Copyright 2015 ghostwords.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+function init_checkboxes() {
+	var els = document.querySelectorAll('.select-all');
+
+	function find_parent(el) {
+		var parent = el;
+		while (['table', 'form'].indexOf(parent.tagName.toLowerCase()) == -1) {
+			parent = parent.parentNode;
+		}
+		return parent;
+	}
+
+	for (var i = 0, count = els.length; i < count; i++) {
+		var child_checks = find_parent(els[i])
+			.querySelectorAll('input[type=checkbox]');
+
+		(function (checks) {
+			els[i].addEventListener('click', function () {
+				for (var i = 0, count = checks.length; i < count; i++) {
+					if (checks[i] != this) {
+						checks[i].checked = this.checked;
+					}
+				}
+			});
+		}(child_checks));
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	init_checkboxes();
+});
